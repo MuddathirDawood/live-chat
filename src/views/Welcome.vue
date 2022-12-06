7819dc23aeb890b31dc2ab900c5d287518bba0a4
MuddathirDawood <dmuddathir@gmail.com>
2022-12-06 13:55:47 +0200

    live chat init

---
+++ src/views/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Welcome'
+}
+
+</script>
+
+<style>
+  .welcome{
+    text-align: center;
+    padding: 20px 0;
+  }
+</style>
