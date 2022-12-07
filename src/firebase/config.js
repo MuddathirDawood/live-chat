78973c3965bf5a04417e9bf33d2b08b1b8417433
MuddathirDawood <dmuddathir@gmail.com>
2022-12-07 08:52:31 +0200

    setup Auth, finish login&signup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAr0ovgkqjQhzDyPYcQAbQhI0aEDFQzW-8",
+    authDomain: "md-vue-firebase.firebaseapp.com",
+    projectId: "md-vue-firebase",
+    storageBucket: "md-vue-firebase.appspot.com",
+    messagingSenderId: "460842892356",
+    appId: "1:460842892356:web:28cb216fbe47ad6d50d011"
+  };
+
+//   INIT FIREBASE
+firebase.initializeApp(firebaseConfig)
+
+const projectAuth = firebase.auth()
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export {projectAuth, projectFirestore, timestamp}
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <div v-if="showLogin">
+      <h2>Login</h2>
+      <LoginForm />
+      <p>No Account Yet? <span @click="showLogin = false">Signup</span></p>
+    </div>
+    <div v-else>
+      <h2>Sign Up</h2>
+      <SignupForm />
+      <p>Already Registered? <span @click="showLogin = true">Login</span></p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import LoginForm from '@/components/LoginForm.vue';
+import SignupForm from '@/components/SignupForm.vue';
+import { ref } from 'vue';
+
 export default {
-  name: 'Welcome'
+    name: "Welcome",
+    components: { SignupForm, LoginForm },
+    setup(){
+      const showLogin = ref(true)
+
+      return { showLogin }
+    }
 }
 
 </script>
@@ -18,4 +38,35 @@ export default {
     text-align: center;
     padding: 20px 0;
   }
+
+  .welcome form{
+    width: 300px;
+    margin: 20px auto;
+  }
+
+  .welcome label{
+    display: block;
+    margin: 20px 0 20px;
+  }
+
+  .welcome input{
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
+
+  .welcome span{
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  .welcome button{
+    margin: 20px auto;
+  }
+
 </style>
